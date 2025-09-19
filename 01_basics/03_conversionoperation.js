e7056a148fae8485d081e0d1dd0b72fb29e8b089
Kaustubh-MhatreGit <kaustubhmaingit@gmail.com>
2025-09-19 08:15:32 +0000

    Conversion of Datatypes

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,37 @@
+let score = "Kaustubh"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+let convertIsLoggedIn = Boolean(isLoggedIn)
+console.log(convertIsLoggedIn);
+
+let empty =""
+let convertEmpty = Boolean(empty)
+console.log(typeof convertEmpty);
+console.log(convertEmpty)
+
+//1 => true; 0 => false
+// "" => false
+// "kaustubh" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
+
+
+
