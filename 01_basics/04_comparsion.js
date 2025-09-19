b32e20572ee72c328d4e7a25ec3492d947aabe3e
Kaustubh-MhatreGit <kaustubhmaingit@gmail.com>
2025-09-19 15:51:56 +0000

    comparison

---
+++ 01_basics/04_comparsion.js
@@ -0,0 +1,24 @@
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);   // Avoid
+// console.log(null == 0);  // Avoid
+// console.log(null >= 0);  // Avoid
+// console.log(null <= 0);  // Avoid
+
+
+// console.log(undefined > 0);   // Avoid
+// console.log(undefined == 0);  // Avoid
+// console.log(undefined >= 0);  // Avoid
+// console.log(undefined <= 0);  // Avoid
+
+// === is called as strict check; It checks values along with datatypes
+
+console.log("2" == 2);
+console.log("2" === 2);
+console.log(15);
