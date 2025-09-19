34d1266bde1495b2be4ecf7efd249bbd6f59938d
Kaustubh-MhatreGit <kaustubhmaingit@gmail.com>
2025-09-19 15:07:21 +0000

    operations

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -1,11 +1,11 @@
 let score = "Kaustubh"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33" => 33
 // "33abc" => NaN
@@ -13,12 +13,12 @@ console.log(valueInNumber);
 
 let isLoggedIn = 1
 let convertIsLoggedIn = Boolean(isLoggedIn)
-console.log(convertIsLoggedIn);
+// console.log(convertIsLoggedIn);
 
 let empty =""
 let convertEmpty = Boolean(empty)
-console.log(typeof convertEmpty);
-console.log(convertEmpty)
+// console.log(typeof convertEmpty);
+// console.log(convertEmpty)
 
 //1 => true; 0 => false
 // "" => false
@@ -26,8 +26,72 @@ console.log(convertEmpty)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// **********************OPEARTIONS***********************
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+// let str1 = "hello"
+// let str2 = " kaustubh"
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log(3 + 4 * 5 % 3);
+
+// console.log(+true);
+// console.log(-true);
+// console.log(+"");
+
+// let hum1, num2, num3
+// num1 = num2 = num3 = 2+2
+// console.log(num1, num2, num3);
+
+// let gameCounter = 100
+// ++gameCounter;
+// console.log(gameCounter);
+
+//*********************PREFIX AND POSTFIX******************/
+
+let x = 3
+const y = x++
+console.log([x, y])
+
+let a = 3
+const b = ++a
+console.log(a, b);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
