135611baf5c286ac03c73ed2d241c63725d1c015
Kaustubh-MhatreGit <kaustubhmaingit@gmail.com>
2025-09-19 07:43:25 +0000

    DataTypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,34 @@
+//"use strict"; //Treat all JS code as newer version
+
+// alert(3+3) // We are using nodejs, not browser
+
+/*console.log(3 
+    +             // code readability should be high
+    3);
+*/
+
+
+console.log(3 + 3);
+console.log("Kaustubh");
+
+let name = "Kaustubh"  //String
+let age = 22  //Number
+let isLoggedIn = false  //Boolean
+let state; //undefined
+let package = null // empty variable
+
+// number=> 2 to power 53
+// bigint 
+// string => "" , ''
+// boolean => true/false
+// null   // its a standalone value
+// undefined
+// symbol => unique
+
+// object
+
+console.log(typeof "Kaustubh"); // output is string
+console.log(typeof null); // output is object
+console.log(typeof undefined); // output is undefined
+
+
