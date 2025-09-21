5ea10ad6dd5dcbf9905285430993291e9386ca77
Kaustubh-MhatreGit <kaustubhmaingit@gmail.com>
2025-09-21 13:02:38 +0000

    String pre

---
+++ 01_basics/05_string.js
@@ -0,0 +1,7 @@
+const name = "Kaustubh"
+const repoCount = 50
+
+console.log(name + repoCount + " Value" )   //outdated
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); //Current Way
+
